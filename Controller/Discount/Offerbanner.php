41bace86e43f5f2bf19aa6da160963a7f064f81c
Andrew Kamau <andrew@talitu.com>
2025-03-19 16:34:18 +0000

    2.6.8

--- Controller/Discount/Offerbanner.php
+++ Controller/Discount/Offerbanner.php
@@ -14,6 +14,7 @@ use Magento\Framework\App\Request\InvalidRequestException;
 use Magento\Framework\App\RequestInterface;
 use Magento\Framework\Controller\Result\JsonFactory;
 use Magento\Framework\Controller\ResultInterface;
+use Magento\Framework\Escaper;
 use Magento\Quote\Api\Data\CartInterface;
 use Magento\Store\Model\StoreManagerInterface;
 use Psr\Log\LoggerInterface;
@@ -54,10 +55,11 @@ class Offerbanner implements ActionInterface, HttpPostActionInterface, CsrfAware
     /** @var PriceConverter $priceConverter */
     private $priceConverter;
 
-    /**
-     * @var LoggerInterface
-     */
+    /** @var LoggerInterface */
     private $logger;
+
+    /** @var Escaper */
+    private $escaper;
 
     public function __construct(
         Context $context,
@@ -67,7 +69,8 @@ class Offerbanner implements ActionInterface, HttpPostActionInterface, CsrfAware
         JsonFactory $jsonResultFactory,
         CheckoutSessionRepository $checkoutSessionRepository,
         PriceConverter $priceConverter,
-        LoggerInterface $logger
+        LoggerInterface $logger,
+        Escaper $escaper
     ) {
         $this->jsonResultFactory = $jsonResultFactory;
         $this->request = $context->getRequest();
@@ -77,6 +80,7 @@ class Offerbanner implements ActionInterface, HttpPostActionInterface, CsrfAware
         $this->checkoutSessionRepository = $checkoutSessionRepository;
         $this->priceConverter = $priceConverter;
         $this->logger = $logger;
+        $this->escaper = $escaper;
     }
 
     public function execute(): ResultInterface
@@ -95,10 +99,11 @@ class Offerbanner implements ActionInterface, HttpPostActionInterface, CsrfAware
             $cartAmount = $this->priceConverter->minorUnitAmount($this->quote->getGrandTotal());
             $cartId = ($this->quote->getId() ?: 'unknown-' . time());
             $cartItems = $this->getCartItems();
+            $phoneNumber = $this->getPhoneNumber();
             $page = 'checkout';
             $data = [
                 'title' => '<div class="super-payment-method-title"><super-payment-method-title cartAmount="' . $cartAmount . '" page="' . $page . '" cartId="' . $cartId . '" cartItems="' . $this->cartItemsEncode($cartItems) . '"></super-payment-method-title></div>',
-                'description' => '<super-checkout amount="' . $cartAmount . '" checkout-session-token="' . $superCheckoutSessionToken . '"></super-checkout>',
+                'description' => '<super-checkout amount="' . $cartAmount . '" checkout-session-token="' . $superCheckoutSessionToken . '" phone-number="' . $this->escaper->escapeHtmlAttr($phoneNumber) . '"></super-checkout>',
             ];
         } catch (Exception $e) {
             $this->logger->error($e->getMessage(), ['exception' => $e]);
@@ -141,6 +146,47 @@ class Offerbanner implements ActionInterface, HttpPostActionInterface, CsrfAware
 
     public function createCsrfValidationException(RequestInterface $request): ?InvalidRequestException
     {
+        return null;
+    }
+
+    private function getPhoneNumber(): string
+    {
+        try {
+            $phoneNumber = '';
+            $billingAddress = $this->quote->getBillingAddress();
+            $phoneNumber = $billingAddress ? $billingAddress->getTelephone() : '';
+            if (empty($phoneNumber)) {
+                $phoneNumber = $this->getCustomerDefaultBillingTelephone();
+            }
+            if (empty($phoneNumber)) {
+                $shippingAddress = $this->quote->getShippingAddress();
+                $phoneNumber = $shippingAddress ? $shippingAddress->getTelephone() : '';
+            }
+        } catch (Throwable $e) {
+            $this->logger->error('[SuperPayment] Offerbanner::getPhoneNumber ' . $e->getMessage(), ['exception' => $e]);
+        }
+
+        return $phoneNumber ?? '';
+    }
+
+    private function getCustomerDefaultBillingTelephone(): ?string
+    {
+        try {
+            $customerId = $this->quote->getCustomerId();
+            if (!$customerId) {
+                return null;
+            }
+
+            $customerAddresses = $this->quote->getCustomer()->getAddresses();
+            foreach ($customerAddresses as $customerAddress) {
+                if ($customerAddress->isDefaultBilling() && $telephone = $customerAddress->getTelephone()) {
+                    return $telephone;
+                }
+            }
+        } catch (Throwable $e) {
+            $this->logger->error('[SuperPayment] Offerbanner::getCustomerDefaultBillingTelephone ' . $e->getMessage(), ['exception' => $e]);
+        }
+
         return null;
     }
 }
